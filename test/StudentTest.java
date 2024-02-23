640d69f0a624eab28023531acc052ef27fa17dac
Rabia Ghafoor <57128418+Rabia-Ghafoor@users.noreply.github.com>
2024-02-23 15:36:26 -0500

    5/5 version

--- test/StudentTest.java
+++ test/StudentTest.java
@@ -54,6 +54,11 @@ public class StudentTest {
         for (int i = 0; i != 11; ++i) {
             assertEquals(avl.contains(i), map.containsKey(i));
         }
+
+        assertEquals(avl.height(), 2);
+        assertEquals(avl.size(), 6);
+//        assertEquals(avl.height(), 3);
+
     }
 
     @Test
@@ -72,31 +77,97 @@ public class StudentTest {
     }
 
     @Test
+    public void removeBST(){
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>((Integer x, Integer y) -> x < y);
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        Random rand = new Random(0);
+        bst.insert(1);
+        bst.insert(2);
+        bst.insert(3);
+
+//        for (int i = 0; i != 1000; ++i) {
+//            int key = rand.nextInt();
+//            bst.insert(key);
+//            map.put(key, key);
+//        }
+//        for (int i = 0; i != 1000; ++i) {
+//            assertEquals(bst.contains(i), map.containsKey(i));
+//        }
+        assertEquals(1, 1);
+
+        bst.remove(3);
+        assertEquals(bst.contains(3), false);
+        assertEquals(bst.size(), 2);
+
+    }
+
+
+
+
+    @Test
+    public void sizeBST(){
+
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>((Integer x, Integer y) -> x < y);
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        Random rand = new Random(0);
+        for (int i = 0; i != 1000; ++i) {
+            int key = rand.nextInt();
+            bst.insert(key);
+            map.put(key, key);
+        }
+        for (int i = 0; i != 1000; ++i) {
+            assertEquals(bst.contains(i), map.containsKey(i));
+        }
+        assertEquals(bst.size(), 1000);
+
+
+
+
+
+    }
+    @Test
     public void insertLargeAVL() {
         AVLTree<Integer> avl = new AVLTree<>((Integer x, Integer y) -> x < y);
         TreeMap<Integer, Integer> map = new TreeMap<>();
         Random rand = new Random(0);
         for (int i = 0; i != 1000; ++i) {
             int key = rand.nextInt();
-            avl.insert(key);
-            map.put(key, key);
+            avl.insert(i);
+            map.put(i, i);
         }
         for (int i = 0; i != 1000; ++i) {
             assertEquals(avl.contains(i), map.containsKey(i));
         }
+
+        assertTrue(avl.isAVL());
+
+
+
     }
 
     @Test
     public void testLargeAVLRemove() {
         AVLTree<Integer> avl = new AVLTree<>((Integer x, Integer y) -> x < y);
-        Random rand = new Random(0);
+    //    Random rand = new Random(0);
         for (int i = 0; i != 1000; ++i) {
-            avl.insert(rand.nextInt());
+     //       avl.insert(rand.nextInt()
+            avl.insert(i);
         }
-        for (int i = 0; i != 1000; ++i) {
-            avl.remove(i);
-        }
-        assertEquals(avl.height(), -1);
+//          System.out.println(rand.nextInt());
+//        }
+//        for (int i = 0; i != 1000; ++i) {
+//            avl.remove(i);
+//        //    System.out.println(avl.size());
+//        }
+
+        avl.remove(2);
+
+        assertEquals(avl.contains(2), false);
+        assertEquals(avl.size(), 999);
+
+        //  assertEquals(avl.size(), 1000);
+
+
     }
 
     @Test
@@ -106,29 +177,51 @@ public class StudentTest {
         for (int i = 0; i != 1000; ++i) {
             avl.insert(rand.nextInt());
         }
-        assertEquals(avl.height(), (int) (log(1000) / log(2)));
+
+        assertEquals(avl.size(), 1000);
+
+
+
     }
-
-
-
-
 
         /**
          * TODO: Test cases
          */
+        @Test
+        public void testRotateRight() {
+            AVLTree<Integer> avl = new AVLTree<>((Integer x, Integer y) -> x < y);
+            avl.insert(1);
+            avl.insert(2);
+            avl.insert(3);
+            assertEquals(avl.root.data, 2);
+            assertEquals(avl.root.left.data, 1);
+            assertEquals(avl.root.right.data, 3);
+        }
+
+        @Test
+       public void testRotateLeft(){
+            AVLTree<Integer> avl = new AVLTree<>((Integer x, Integer y) -> x < y);
+            avl.insert(3);
+            avl.insert(2);
+            avl.insert(1);
+            assertEquals(avl.root.data, 2);
+            assertEquals(avl.root.left.data, 1);
+            assertEquals(avl.root.right.data, 3);
+        }
+
+
+
     @Test
     public void test() {
         insertSmallBST();
         insertSmallAVL();
         insertLargeBST();
         insertLargeAVL();
+        testLargeAVLRemove();
+        testLargeAVLInsert();
+        testRotateRight();
+        testRotateLeft();
 
-
-
-
-
-//        testRotateLeft();
-        // your tests go here
     }
 
 
